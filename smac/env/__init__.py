42b5d1f1785dbfcad7aba85e99f51aacfbb4d764
benellis3 <benellis@robots.ox.ac.uk>
2022-03-27 12:39:20 +0100

    fix: Add capability wrapper to __init__

--- smac/env/__init__.py
+++ smac/env/__init__.py
@@ -4,5 +4,6 @@ from __future__ import print_function
 
 from smac.env.multiagentenv import MultiAgentEnv
 from smac.env.starcraft2.starcraft2 import StarCraft2Env
+from smac.env.starcraft2.wrapper import StarCraftCapabilityEnvWrapper
 
-__all__ = ["MultiAgentEnv", "StarCraft2Env"]
+__all__ = ["MultiAgentEnv", "StarCraft2Env", "StarCraftCapabilityEnvWrapper"]
