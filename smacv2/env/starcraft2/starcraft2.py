16d4ad8d528cc50dd75c2812558c3b7c566304ff
Jonathan Cook <jcook@jonnys-mbp.ddns.eng.ox.ac.uk>
2023-04-20 17:40:02 +0100

    fix indexing

--- smacv2/env/starcraft2/starcraft2.py
+++ smacv2/env/starcraft2/starcraft2.py
@@ -1490,7 +1490,7 @@ class StarCraft2Env(MultiAgentEnv):
         ally_feats = np.zeros(ally_feats_dim, dtype=np.float32)
         own_feats = np.zeros(own_feats_dim, dtype=np.float32)
 
-        ind = 6
+        ind = 4
         if (
             unit.health > 0 and self.obs_starcraft
         ):  # otherwise dead, return all zeros
